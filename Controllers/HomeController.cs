fc92c9cfad234f5561d4515f176fcc88059431d6
GerardGhub <singian28@gmail.com>
2023-01-02 18:22:19 +0800

    Adding Css Files

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,15 +1,24 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Services;
 
 namespace DISample.Controllers
 {
     public class HomeController : Controller
     {
-        private readonly CitiesService citiesService;
+        private readonly CitiesService _citiesService;
+
+
+        //contructor
+        public HomeController()
+        {
+            _citiesService = new CitiesService();
+        }
 
         [Route("/")]
         public IActionResult Index()
         {
-            return View();
+          List<string> cities =  _citiesService.GetCities();
+            return View(cities);
         }
     }
 }
