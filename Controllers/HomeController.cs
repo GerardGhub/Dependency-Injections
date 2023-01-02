e346777a05b3a62e3468e72f17829c8be95b98a6
GerardGhub <singian28@gmail.com>
2023-01-02 17:44:41 +0800

    Adding Class Library and creating a reference variable

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace DISample.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly CitiesService citiesService;
+
+        [Route("/")]
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
